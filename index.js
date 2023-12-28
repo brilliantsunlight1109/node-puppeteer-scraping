0c8efa1dd03bc8a67ad38c7b9519335d42c1ac56
brilliantsunlight1109 <brilliantsunlight1109@gmail.com>
2023-12-28 22:33:14 +0900

    page.url added

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const browserObject = require('./browser');
-const scraperController = require('./pageController');
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--disable-setuid-sandbox"],
+    'ignoreHTTPSErrors': true
+});
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  });
+  await page.goto('https://salonboard.com/login/');
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+  await page.type('input.w240', 'CD66356');
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance)
+  await page.type('input.loginPwInput', 'bridge123!!');
+
+  await page.click('div.columnBlock.mt46.ml15 > a');
+
+  console.log("button click");
+
+const handleUrlChange = async (newUrl) => {
+    if (newUrl === 'https://salonboard.com/CLP/bt/top/') {
+      console.log('URL search:');
+      await page.goto('https://salonboard.com/CNB/draft/styleEdit/');
+    }
+  };
+  
+  // Attach the event listener to 'framenavigated' event
+  page.on('framenavigated', async (frame) => {
+    const newUrl = await frame.url();
+    console.log("newUrl: ", newUrl);
+    await handleUrlChange(newUrl);
+  });
+
+})();
